f884469a054ef20d3cc4955b474f894187038ef2
billkwon <fd9020@gmail.com>
2018-06-08 19:00:45 +0900

    remove api keys

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBX1Gd7nNr2XdO8P_R5078YKs6NaeGSvSA",
-    authDomain: "lgtvconnect-bk.firebaseapp.com",
-    databaseURL: "https://lgtvconnect-bk.firebaseio.com",
-    projectId: "lgtvconnect-bk",
-    storageBucket: "lgtvconnect-bk.appspot.com",
-    messagingSenderId: "5011567640"
+
 };
 
 firebase.initializeApp(config);
